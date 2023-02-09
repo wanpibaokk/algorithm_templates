52dda2964a9a76f33837ceb4a771be371d81ed38
wenjh_mac <wenjhkk@gmail.com>
2023-02-09 11:23:49 +0800

    2023.1.15 update

--- Chapter1.java
+++ Chapter1.java
@@ -57,6 +57,75 @@
  }
  return l;
 
+ * 高精度：由于数字位数过多超过了一般数据类型可存的长度
+ * 一般使用数组来高精度的表示这个超长的数字
+ * 主要涉及四类情况：
+ * 高精度加法：两个高精度数字相加
+ * 高精度减法：两个高精度数字相减
+ * 高精度乘低精度：一个用数组表示，一个用int表示
+ * 高精度除以低精度：一个用数组表示，一个用int表示
+ *
+ * 高精度加法
+ List<Integer> add(List<Integer> A, List<Integer> B) {
+     int n = A.size(), m = B.size();
+     List<Integer> C = new ArrayList<>();
+     int t = 0;
+     for (int i = 0; i < n || i < m; i++) {
+         if (i < n) t += A.get(i);
+         if (i < m) t += B.get(i);
+         C.add(t % 10);
+         t /= 10;
+     }
+     if (t > 0) C.add(t);
+     return C;
+ }
+
+ * 高精度减法
+ * 先判断AB谁更大，模板默认 A > B > 0
+ * 结果可能存在前导零  123 - 120 = 003
+ List<Integer> sub(List<Integer> A, List<Integer> B) {
+     int n = A.size(), m = B.size();
+     List<Integer> C = new ArrayList<>();
+     for (int i = 0, t = 0; i < n; i++) {
+         t += A.get(i);
+         if (i < m) t -= B.get(i);
+         C.add((t + 10) % 10);
+         if (t < 0) t = -1;
+         else t = 0;
+     }
+     return C;
+ }
+
+ * 高精度乘低精度
+ List<Integer> mul(List<Integer> A, int b) {
+     int n = A.size();
+     List<Integer> C = new ArrayList<>();
+     for (int i = 0, t = 0; i < n || t > 0; i++) {
+         if (i < n) t += A.get(i) * b;
+         C.add(t % 10);
+         t /= 10;
+     }
+     return C;
+ }
+
+ * 高精度除以低精度
+ * 结果可能存在前导零，并且要注意结果顺序和另外三种情况相反
+ * r为余数，初始为0
+ * java是值传递，所以余数不好返回，做题直接写在main处理
+ List<Integer> div(List<Integer> A, int b, int r) {
+     int n = A.size();
+     List<Integer> C = new ArrayList<>();
+     for (int i = n - 1; i >= 0; i--) {
+         r = r * 10 + A.get(i);
+         C.add(r / b);
+         r %= b;
+     }
+ }
+
+ * 前缀和（一维、二维）、差分（一维和二维）
+
+ *
+
  */
 public class Chapter1 {
     public static void main(String[] args) {
--- Main.java
+++ Main.java
@@ -1,10 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] ss = reader.readLine().split(" ");
+        int n = Integer.parseInt(ss[0]), m = Integer.parseInt(ss[1]), k = Integer.parseInt(ss[2]);
+        int[][] diff = new int[n + 10][m + 10];
+        for (int i = 1; i <= n; i++) {
+            ss = reader.readLine().split(" ");
+            for (int j = 1; j <= m; j++) {
+                insert(diff, i, j, i, j, Integer.parseInt(ss[j - 1]));
+            }
+        }
+        while (k-- > 0) {
+            ss = reader.readLine().split(" ");
+            int x1 = Integer.parseInt(ss[0]), y1 = Integer.parseInt(ss[1]);
+            int x2 = Integer.parseInt(ss[2]), y2 = Integer.parseInt(ss[3]);
+            int c = Integer.parseInt(ss[4]);
+            insert(diff, x1, y1, x2, y2, c);
+        }
+        for (int i = 1; i <= n; i++) {
+            StringBuffer sb = new StringBuffer();
+            for (int j = 1; j <= m; j++) {
+                diff[i][j] = diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1] + diff[i][j];
+                sb.append(diff[i][j]).append(" ");
+            }
+            System.out.println(sb.toString());
+        }
 
+    }
+    static void insert(int[][] diff, int x1, int y1, int x2, int y2, int c) {
+        diff[x1][y1] += c;
+        diff[x2 + 1][y1] -= c;
+        diff[x1][y2 + 1] -= c;
+        diff[x2 + 1][y2 + 1] += c;
     }
 }
