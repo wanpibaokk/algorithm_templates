17f78a92fe2b09d697b3e97609ff34b210621424
wenjh_mac <wenjhkk@gmail.com>
2023-07-23 13:11:48 +0800

    2023.7.23 update

--- acwing/Chapter1.java
+++ acwing/Chapter1.java
@@ -1,3 +1,5 @@
+package acwing;
+
 /**
  * 包括排序、二分、高精度、前缀和与差分、双指针算法、位运算、离散化、区间合并等内容。
  *
@@ -69,18 +71,26 @@
  List<Integer> add(List<Integer> A, List<Integer> B) {
      int n = A.size(), m = B.size();
      List<Integer> C = new ArrayList<>();
-     int t = 0;
-     for (int i = 0; i < n || i < m; i++) {
+     for (int i = 0, t = 0; i < n || i < m || t > 0; i++) {
          if (i < n) t += A.get(i);
          if (i < m) t += B.get(i);
          C.add(t % 10);
          t /= 10;
      }
-     if (t > 0) C.add(t);
      return C;
  }
 
  * 高精度减法
+ boolean cmp(List<Integer> A, List<Integer> B) {
+     int n = A.size(), m = B.size();
+     if (n != m) return n > m;
+     for (int i = n - 1; i >= 0; i--) {
+         if (A.get(i) != B.get(i)) {
+            return A.get(i) > B.get(i);
+        }
+     }
+     return true;
+ }
  * 先判断AB谁更大，模板默认 A > B > 0
  * 结果可能存在前导零  123 - 120 = 003
  List<Integer> sub(List<Integer> A, List<Integer> B) {
@@ -123,8 +133,58 @@
  }
 
  * 前缀和（一维、二维）、差分（一维和二维）
-
  *
+ * 一维前缀和
+ int[] sum = new int[n + 1];
+ for (int i = 1; i <= n; i++) sum[i] = sum[i - 1] + nums[i - 1];
+
+ * 二维前缀和
+ int[][] sum = new int[n + 1][m + 1];
+ for (int i = 1; i <= n; i++) {
+     for (int j = 1; j <= m; j++) {
+        sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + nums[i - 1][j - 1];
+     }
+ }
+
+ * 一维差分
+ int[] diff = new int[n + 1];
+ * [l, r]范围增加c
+ diff[l] += c;
+ diff[r + 1] -= c;
+
+ * 二维差分
+ int[][] diff = new int[n + 5][m + 5]
+ * x1, y1  到  x2, y2 矩形范围内增加c
+ diff[x1][y1] += c;
+ diff[x2 + 1][y1] -= c;
+ diff[x1][y2 + 1] -= c;
+ diff[x2 + 1][y2 + 1] += c;
+
+ * 位运算
+ lowbit的作用：100010 得到 10
+ 应用：树状数组    求二进制中1的个数
+ lowbit(x) {
+    return x & (-x);
+ }
+ 求二进制中1的个数也可以用Brian Kernighan算法
+ x &= (x - 1) 消去最后的1
+
+ * 离散化
+ * 特点：范围很大，但是值很稀疏
+ * 由于范围很大，无法直接创建那么大的数组，所以需要离散化，将值映射到更小的范围
+ * 思路：去重，排序。然后通过二分找到位置，位置即为离散化后的值。
+ list = new ArrayList<>(new HashSet<>(list));
+ Collections.sort(list);
+
+ int find(List<Integer> list, int x) {
+     int l = 0, r = list.size() - 1;
+     while (l < r) {
+         int m = l + r >> 1;
+         if (list.get(m) >= x) r = m;
+         else l = m + 1;
+     }
+     return l;
+ }
 
  */
 public class Chapter1 {
---
+++ acwing/Chapter2.java
@@ -0,0 +1,8 @@
+package acwing;
+
+/**
+ * @author wenjiahao <wenjhkk@gmail.com>
+ * Created on 2023-07-06
+ */
+public class Chapter2 {
+}
